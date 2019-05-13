b887811d9fae6b5d6775ebaba54f9b681659aedc
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-13 05:22:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+  shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -30,7 +30,10 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
+                  "muskets" => 2
+                  "gun powder" => 4
                  }
+                 return shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
